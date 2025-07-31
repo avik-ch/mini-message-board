e14a223967ef59890c32a3a6ba31cb928833e7c3
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-07-31 13:25:55 -0500

    Set up preliminary messages

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const indexRouter = require('./routes/indexRouter');
-
+require("dotenv").config();
+const express = require("express");
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+const newMessageRouter = require("./routes/newMessageRouter");
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // listen
 const PORT = process.env.PORT || 8080;
@@ -11,4 +14,5 @@ app.listen(PORT, () => {
   console.log(`App is live. Listening on port ${PORT}.`);
 });
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
